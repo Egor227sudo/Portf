d2b37c112b5cd49419a66a1aac3088948e29c786
myPortF <71255005+Egor227sudo@users.noreply.github.com>
2021-04-25 11:31:38 +0300

    some bb

---
+++ edison/site38/main.js
@@ -0,0 +1,9 @@
+const clip = document.querySelectorAll('.container .box video');
+for(let i = 0; i < clip.length; i++){
+    clip[i].addEventListener('mouseenter', function(e){
+        clip[i].play();
+    });
+    clip[i].addEventListener('mouseout', function(e){
+        clip[i].pause();
+    });
+}
